c85dee469281a99641f23c2e30da69fde0ed02d5
Stas Zytkiewicz <stas@childsplay.mobi>
2026-03-04 10:16:23 +0100

    Updated from SP

--- nmkivy.sh
+++ nmkivy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # determine if we are on XF OS or not
-if test -d "/opt/App/.venv3.8"; then
+if test -d "/opt/App/.venv3.12"; then
+    py_ex="/opt/App/.venv3.12/bin/python3.12"
+elif test -d "/opt/App/.venv3.8"; then
     py_ex="/opt/App/.venv3.8/bin/python3.8"
-elif test -d "/opt/App/.venv3.11"; then
-    py_ex="/opt/App/.venv3.11/bin/python3.11"
 else
     py_ex="/usr/bin/python3.6"
 fi
+
 
 #we must kill btp as Kivy is a bit borg-ish about multiple windows
 # we kill everything that's running the btp stuff
